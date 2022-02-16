88e3d20f02f815624f09b372eadaa1260a6f33c2
David <begala260@gmail.com>
2022-02-16 09:57:57 -0600

    Segunda pantalla

--- Practica2/ViewController.swift
+++ Practica2/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nombreTF: UIStackView!
+    @IBOutlet weak var edadTF: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func enviarBtn(_ sender: UIButton) {
+        print("Enviar")
+        performSegue(withIdentifier: "enviarTexto", sender: self)
+    }
+    
 }
 
---
+++ Practica2/ViewControllerDos.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerDos.swift
+//  Practica2
+//
+//  Created by mac16 on 16/02/22.
+//
+
+import UIKit
+
+class ViewControllerDos: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
